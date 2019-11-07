ee33e1a2aebc7bb7c70c0a306a8507516ad57df8
Pedro <pedrodacunha1217@gmail.com>
2019-11-07 11:53:34 +0100

    prepare presentation slides

--- helpers.py
+++ helpers.py
@@ -5,7 +5,7 @@ import math
 
 import matplotlib.pyplot as plt
 
-def generate_random_simulation(min_x, max_x, min_y, max_y, n_steps, momentum):
+def generate_random_simulation(min_x, max_x, min_y, max_y, n_steps, momentum=0.9):
     # generate starting position
     start_x = min_x + (max_x - min_x) * torch.rand(1)
     start_y = min_y + (max_y - min_y) * torch.rand(1)
@@ -65,63 +65,23 @@ def generate_features(dist, n_samples):
     
     return features
 
-class RandomSimulation(Dataset):
-    
-    def __init__(self, n_simulations, n_steps, min_x, min_y, max_x, max_y, anchors_pos, momentum=0.9, n_rssi_measurements=10):
-        self.n_simulations = n_simulations
-        self.n_steps = n_steps
-        self.min_x = min_x
-        self.max_x = max_x
-        self.min_y = min_y
-        self.max_y = max_y
-        
-        # generate random simulations
-        self.simulations = torch.stack([generate_random_simulation(min_x, max_x, min_y, max_y, n_steps, momentum) for i in range(n_simulations)])
-        
-        # generate random anchor positions
+class SimulationData:
+    def __init__(self, simulations, anchors_pos, momentum=0.9, n_rssi_measurements=10):
+        self.simulations = simulations
         self.anchors_pos = anchors_pos
+        self.n_simulations, self.n_steps, _ = self.simulations.size()
         
         # generate signals from the anchors distances
         self.distances = self.simulations[..., None, :].sub(self.anchors_pos).pow(2).sum(dim=3).sqrt() + 1e-2
         self.features = generate_features(self.distances, n_rssi_measurements)
         
-    @classmethod
-    def from_file(cls, path):
-        loaded_file = torch.load(path)
-        self = cls.__new__(cls)
-        self.n_simulations = loaded_file['n_simulations']
-        self.n_steps = loaded_file['n_steps']
-        self.min_x = loaded_file['min_x']
-        self.max_x = loaded_file['max_x']
-        self.min_y = loaded_file['min_y']
-        self.max_y = loaded_file['max_y']
-        self.simulations = loaded_file['simulations']
-        self.anchors_pos = loaded_file['anchors_pos']
-        self.features = loaded_file['features']
-        return self 
-        
     def __len__(self):
-        return self.n_simulations
+        return self.simulations.size(0)
 
     def __getitem__(self, idx):
         return self.features[idx], self.simulations[idx]
         
-    def save(self, path):
-        to_save = {
-            'n_simulations': self.n_simulations,
-            'n_steps': self.n_steps,
-            'min_x': self.min_x,
-            'max_x': self.max_x,
-            'min_y': self.min_y,
-            'max_y': self.max_y,
-            'simulations': self.simulations,
-            'anchors_pos': self.anchors_pos,
-            'features': self.features
-        }
-        torch.save(to_save, path)
-        
     def plot_simulation(self, positions, ax=plt):
-        
         ax.grid(True)
         ax.axis('equal')
         ax.plot(*positions.T)
